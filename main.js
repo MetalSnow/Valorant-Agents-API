4acc7ebabea55324c5194c737ea6d864e6c8ee2c
MetalSnow <yasuotrend@gmail.com>
2024-01-08 20:54:42 +0800

    Version 2

--- main.js
+++ main.js
@@ -1,16 +1,25 @@
 const express = require("express");
-const app = express();
-require("dotenv").config();
+const cors = require("cors");
 const agentsRoutes = require("./routes/agentsRoute");
 const usersRoutes = require("./routes/usersRoute");
 const errorHandler = require("./Middlewares/ErrorHandler");
 const connectDB = require("./config/connectionDB");
 
+const app = express();
+require("dotenv").config();
+
 const port = process.env.PORT || 2026;
 
 connectDB();
 
+// Access to one website only!
+var corsOptions = {
+  origin: "http://example.com", //frontend website
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
 app.use(express.json());
+app.use(cors(corsOptions));
 app.use("/api/valorant/agents", agentsRoutes);
 app.use("/api/valorant/users", usersRoutes);
 app.use(errorHandler);
